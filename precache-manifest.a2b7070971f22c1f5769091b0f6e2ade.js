7f91aa958833c016805992a126a5c320d3ba7dae
Zack <zack.komo98@gmail.com>
2020-08-19 19:53:23 -0500

    first commit

---
+++ precache-manifest.a2b7070971f22c1f5769091b0f6e2ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa973003ca88d77176d3af34b0e41fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db67501c54cb8b663b0f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c0fb8d7cdfac75bb4d21",
+    "url": "/static/js/2.fbd0751c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fbd0751c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db67501c54cb8b663b0f",
+    "url": "/static/js/main.1e225559.chunk.js"
+  },
+  {
+    "revision": "933685e1f076567e01a2",
+    "url": "/static/js/runtime-main.f907a9e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
